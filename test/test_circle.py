729c26cb88b4cc495bfb3b521ec3953b3fc131b8
Patrick Sheehan <psheehan287@gmail.com>
2020-03-19 14:42:43 -0500

    Now use "AMR" style grid for the circle test example.

--- test/test_circle.py
+++ test/test_circle.py
@@ -10,12 +10,15 @@ import time
 
 # Make an image.
 
-grid = pyDOE.lhs(2, samples=30000)
+r, phi = numpy.meshgrid(numpy.hstack((numpy.linspace(0.,0.98,15), \
+        numpy.linspace(0.98,1.02,30), numpy.linspace(1.02,2.,15))), \
+        numpy.linspace(0.,2*numpy.pi,100))
 
-x = grid[:,0] * 4 - 2.
-y = grid[:,1] * 4 - 2.
+r = r.reshape((r.size,))*numpy.random.uniform(0.999,1.001,r.size)
+phi = phi.reshape((phi.size,))*numpy.random.uniform(0.999,1.001,phi.size)
 
-r = (x**2 + y**2)**0.5
+x = r * numpy.cos(phi)
+y = r * numpy.sin(phi)
 
 flux = numpy.where(r < 1., 1., 0.)
 
@@ -28,11 +31,11 @@ plt.show()
 
 # Do the Fourier transform with TrIFT
 
-u = numpy.linspace(0.,10.,1000)
+u = numpy.linspace(0.001,10.,1000)
 v = numpy.repeat(0., 1000)
 
 t1 = time.time()
-vis = trift.trift_c(x, y, flux, u, v, 0.25, 0.25)
+vis1 = trift.trift_c(x, y, flux, u, v, 0.25, 0.25)
 t2 = time.time()
 print(t2 - t1)
 
@@ -43,6 +46,7 @@ vis_analytic = scipy.special.jv(1, 2*numpy.pi*u) / u * numpy.exp(2*numpy.pi*\
 
 # Finally, plot the visibilities.
 
-plt.plot(u, vis.real, "k-")
-plt.plot(u, vis_analytic.real, "b.-")
+plt.plot(u, vis.real, "k.-")
+plt.plot(u, vis1.real, "b.-")
+plt.plot(u, vis_analytic.real, "r-")
 plt.show()
