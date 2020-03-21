59a4f6127c74ffd3f7cfb39432d4c720948124e3
Patrick Sheehan <psheehan287@gmail.com>
2020-03-21 09:53:02 -0500

    Moved specification of gcc compiler to compile so to be more agnostic - users can specify for themselves (as compile is my own personal installation script).

--- setup.py
+++ setup.py
@@ -5,9 +5,6 @@ import os
 
 import numpy
 
-os.environ["CC"] = "gcc-9"
-os.environ["CXX"] = "gcc-9"
-
 trift = Extension("trift.trift",sources=["trift/trift.pyx","src/trift.cc"],\
         include_dirs=[numpy.get_include(),"./include"], language="c++", \
         extra_compile_args=['-std=c++11','-O3','-fopenmp'],\
