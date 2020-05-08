30bebee95863051c105bab9fa1233f613f0c42dc
Patrick Sheehan <psheehan287@gmail.com>
2020-05-08 13:37:52 -0500

    Added '-march=native' flag to compile options as this seems to speed everything up without creating errors.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ import numpy
 trift = Extension("trift.trift",sources=["trift/trift.pyx","src/trift.cc"],\
         include_dirs=[numpy.get_include(),"./include",\
         "./delaunator-cpp/include"], language="c++", \
-        extra_compile_args=['-std=c++11','-Ofast'],\
-        extra_link_args=["-std=c++11",'-Ofast'])
+        extra_compile_args=['-std=c++11','-Ofast','-march=native'],\
+        extra_link_args=["-std=c++11",'-Ofast','-march=native'])
 
 setup(cmdclass = {'build_ext': build_ext}, ext_modules = [trift])
